f68d415f8a4867aeee03e8de988983f9a1e5950f
Sascha Lamber <sascha.lamber@gmail.com>
2016-01-06 10:35:30 +0100

    added StartActivity with layout

--- app/src/main/java/de/lamber/sascha/einmaleins/MainActivity.java
+++ app/src/main/java/de/lamber/sascha/einmaleins/MainActivity.java
@@ -2,12 +2,19 @@ package de.lamber.sascha.einmaleins;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+        imageView.setImageResource(R.mipmap.positive);
+
     }
 }
